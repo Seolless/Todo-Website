7a2808f9e5251dc8cecbf9537ecc3779f81d00bb
Seolless <angelzimain@gmail.com>
2024-11-20 16:50:12 +0100

    Added editing and better creation menu.

--- src/index.js
+++ src/index.js
@@ -72,11 +72,7 @@ class Todos {
         break;
       }
     }
-    const removeThis = document.querySelector(".todo-menu-wrapper");
-    document
-      .getElementById("createTodo")
-      .removeEventListener("click", () => this.addTodo());
-    removeThis.remove();
+    this.closeMenu();
   }
   removeTodo(e) {
     const project = e.target.parentNode.closest("ul");
@@ -92,15 +88,77 @@ class Todos {
     this.saveTodos();
     this.render();
   }
+  editTodo(index, projectId) {
+    const title = document.getElementById("createTitle").value;
+    const description = document.getElementById("createDescription").value;
+    const project = projectId;
+    const date = document.getElementById("createDate").value;
+    const priority = document.getElementById("createPriority").value;
+    for (let i = 0; i < this.todoList.length; i++) {
+      const element = this.todoList[i];
+      if (element.name == projectId) {
+        element.todos[index].title = title;
+        element.todos[index].description = description;
+        element.todos[index].date = date;
+        element.todos[index].priority = priority;
+        break;
+      }
+    }
+    this.closeMenu();
+    this.saveTodos();
+    this.render();
+  }
+  closeMenu() {
+    if (document.getElementById("editTodo")) {
+      document
+        .getElementById("editTodo")
+        .removeEventListener("click", () => this.editTodo());
+    } else if (document.getElementById("createTodo")) {
+      document
+        .getElementById("createTodo")
+        .removeEventListener("click", () => this.addTodo());
+    }
+
+    const removeMenu = document.querySelector(".todo-menu-wrapper");
+    removeMenu.remove();
+  }
+  renderEditMenu(e) {
+    const project = e.target.parentNode.closest("ul");
+    const projectId = project.id.replace("project-", "");
+    const index = e.target.id.replace("edit-btn-", "");
+    let todo;
+    for (let i = 0; i < this.todoList.length; i++) {
+      const element = this.todoList[i];
+      if (element.name == projectId) {
+        todo = element.todos[index];
+      }
+    }
+
+    const editMenu = document.createElement("div");
+    editMenu.classList.add("todo-menu-wrapper");
+    editMenu.innerHTML = `<div class=todo-menu><div id="closeMenu">✖</div><div class="menu-section"><h1 class="menu-text">Title</h1><input type="text" placeholder="Title" class="input-field" id="createTitle"></div><div class="menu-section"><h1 class="menu-text">Description</h1><input type="text" placeholder="Description" class="input-field" id="createDescription"></div><div class="menu-section"><h1 class="menu-text">Date / Time</h1><div class="flex time-wrapper"><input type="date" class="text-center input-field" id="createDate"><input type="time" class="text-center input-field" id="createTime"></div></div><div class="menu-section"><h1 class="menu-text">Priority</h1><input type="range" class="input-field" id="createPriority"></div><div class="menu-section"><button class="input-field" id="editTodo">Edit</button></div></div>`;
+    document.getElementById("content").appendChild(editMenu);
+
+    document.getElementById("createTitle").value = todo.title;
+    document.getElementById("createDescription").value = todo.description;
+    document.getElementById("createDate").value = todo.date;
+    document.getElementById("createPriority").value = todo.priority;
+    document
+      .getElementById("editTodo")
+      .addEventListener("click", () => this.editTodo(index, projectId));
+  }
   renderCreateMenu(e) {
     const projectId = e.target.parentNode.id.replace("project-", "");
     const createMenu = document.createElement("div");
     createMenu.classList.add("todo-menu-wrapper");
-    createMenu.innerHTML = `<div class=todo-menu><input type="text" placeholder="Title" id="createTitle"></input><input type="text" placeholder="Description" id="createDescription"></input><input type="date" id="createDate"></input><input type="range" id="createPriority"></input><button id="createTodo">Submit</button></div>`;
+    createMenu.innerHTML = `<div class=todo-menu><div id="closeMenu">✖</div><div class="menu-section"><h1 class="menu-text">Title</h1><input type="text" placeholder="Title" class="input-field" id="createTitle"></div><div class="menu-section"><h1 class="menu-text">Description</h1><input type="text" placeholder="Description" class="input-field" id="createDescription"></div><div class="menu-section"><h1 class="menu-text">Date / Time</h1><div class="flex time-wrapper"><input type="date" class="text-center input-field" id="createDate"><input type="time" class="text-center input-field" id="createTime"></div></div><div class="menu-section"><h1 class="menu-text">Priority</h1><input type="range" class="input-field" id="createPriority"></div><div class="menu-section"><button class="input-field" id="createTodo">Add</button></div></div>`;
     document.getElementById("content").appendChild(createMenu);
     document
       .getElementById("createTodo")
       .addEventListener("click", () => this.addTodo(projectId));
+    document
+      .getElementById("closeMenu")
+      .addEventListener("click", () => this.closeMenu());
   }
   render() {
     if (this.el.childNodes) {
@@ -132,7 +190,7 @@ class Todos {
             this.removeTodo(e);
           });
           const editButton = todo.querySelector(".edit-btn");
-          editButton.addEventListener("click", (e) => this.editTodo(e));
+          editButton.addEventListener("click", (e) => this.renderEditMenu(e));
         }
       }
     }
